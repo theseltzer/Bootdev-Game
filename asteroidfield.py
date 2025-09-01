70522eb529bb671cb7ba1cea920cb4798e3181cc
theseltzer <A03hesabim@gmail.com>
2025-09-01 23:59:06 +0300

    Score system update

--- asteroidfield.py
+++ asteroidfield.py
@@ -3,8 +3,9 @@ import random
 from asteroid import Asteroid
 from constants import *
 
-
+#pygame.sprite.Sprite() miras alarak asteriodlerin spawnlarını ayarlıyoruz
 class AsteroidField(pygame.sprite.Sprite):
+    # Asteroidlerin doğabileceği 4 ekran tanımlıyor her kenar için hız vektörü artı konum hesaplama fonksiyonu yazıyor
     edges = [
         [
             pygame.Vector2(1, 0),
@@ -27,25 +28,28 @@ class AsteroidField(pygame.sprite.Sprite):
             ),
         ],
     ]
-
+    # Constructor
     def __init__(self):
+        # oluşturulacak asteroidleri ait olduğu gruba ekliyor, drawable ve updatable
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.spawn_timer = 0.0
+        self.spawn_timer = 0.0 # Asteroid spawn timer
 
-    def spawn(self, radius, position, velocity):
-        asteroid = Asteroid(position.x, position.y, radius)
-        asteroid.velocity = velocity
+    def spawn(self, radius, position, velocity): # spawner radius position ve velocitye göre asteroid spawnlayıcı
+        asteroid = Asteroid(position.x, position.y, radius) # Asteroid sınıfından onun self bilgileriyle asteroid bilgileri tanımlanıyor ( Asteroid sınıfının x,y konumu ve radius bilgisi vardı)
+        asteroid.velocity = velocity # velocityde burada veriyoruz
 
-    def update(self, dt):
+    def update(self, dt): #Update etme fonksiyonu
+        # delta süresine göre update, zamanlayıcı yani
         self.spawn_timer += dt
+        # eğer timer minimum max spawn süresini aşarsa zamanlalyıcı sıfırlanır
         if self.spawn_timer > ASTEROID_SPAWN_RATE:
             self.spawn_timer = 0
 
-            # spawn a new asteroid at a random edge
-            edge = random.choice(self.edges)
-            speed = random.randint(40, 100)
-            velocity = edge[0] * speed
-            velocity = velocity.rotate(random.randint(-30, 30))
-            position = edge[1](random.uniform(0, 1))
-            kind = random.randint(1, ASTEROID_KINDS)
-            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
+            # Sıfırdan sonra asteroid spawn için bilgiler
+            edge = random.choice(self.edges)                           # rastgele kenar
+            speed = random.randint(40, 100)                            # 40 100 arası rastgele hız
+            velocity = edge[0] * speed                                 # kenar vektörü ile hız çarpıp son hız vektörü
+            velocity = velocity.rotate(random.randint(-30, 30))        # hıza açı verir
+            position = edge[1](random.uniform(0, 1))                   # rastgele konum kenarda
+            kind = random.randint(1, ASTEROID_KINDS)                   # ASTEROID_KINDS içinden rastgele asteroid
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity) # bu bilgilerle yeni asteroid spawn
